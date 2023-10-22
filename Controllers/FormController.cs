fca99e456401efe505dbeaaf479af4f35b6100b1
Dahyun (Dany) <iamgo910@gmail.com>
2023-10-22 13:06:29 -0400

    Build Login, SignUp Form

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -5,9 +5,23 @@ namespace Team_Turtle.Controllers
     public class FormController : Controller
     // This will controll the login/sign up form and validate the form
     {
-        public IActionResult Index()
+        public IActionResult SignUp()
         {
             return View();
+        }
+
+        /*
+        [HttpPost]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        */
+
+        public IActionResult Login()
+        {
+            return View();
+
         }
     }
 }
