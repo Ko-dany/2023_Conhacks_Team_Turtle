2d98adb927d9194574fddb3840ff8878288bff81
Dahyun (Dany) <iamgo910@gmail.com>
2023-10-22 11:53:09 -0400

    Set the basic welcome page, add controllers for form/board

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_Turtle.Controllers
+{
+    public class BoardController : Controller
+    // This will control the CRUD of posts for the study group list
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/FormController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_Turtle.Controllers
+{
+    public class FormController : Controller
+    // This will controll the login/sign up form and validate the form
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
